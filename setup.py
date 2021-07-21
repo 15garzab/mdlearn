1a6db03067cb510a63d739137bb8a9a38e9df860
Richard Garza <nunya@business.com>
2021-07-21 00:07:11 -0400

    Corrected setup.py with commas
    Fixed setup.py for install

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mdlearn'
-    version='0.1.0'
-    packages=find_packages(include=['mdlearn'])
-    install_requires=['numpy, pandas, matplotlib, ovito, tensorflow']
+    name='mdlearn',
+    version='0.1.0',
+    packages=find_packages(include=['mdlearn']),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'ovito', 'tensorflow']
 )
